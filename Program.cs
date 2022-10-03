398c2ac686cf6a4afa9483dfbc4d7004e8f26a84
NitroTech 4K <crtraer@gmail.com>
2022-10-03 11:07:52 -0300

    Added methods

--- Program.cs
+++ Program.cs
@@ -8,8 +8,39 @@ namespace HelloWorldMethods1
 {
     internal class Program
     {
+        static int score;
+        static int enemiekillvalue;
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods stuff");
+
+            Console.WriteLine("-------------");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("----------");
+            
+            score = 0;
+
+            
+            enemiekillvalue = 50;
+
+
+
+
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("----------");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Eneime Kill Value: " + enemiekillvalue);
+
+            Console.ReadKey(true);
         }
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("Score:" + score);
+        }
+        
     }
 }
