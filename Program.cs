109538f94c668456d2db265936904260561a664d
NitroTech 4K <crtraer@gmail.com>
2022-10-03 20:42:45 -0300

    Added Kill Enemie and added score multipler

--- Program.cs
+++ Program.cs
@@ -9,15 +9,18 @@ namespace HelloWorldMethods1
     internal class Program
     {
         static int score;
-        static int enemiekillvalue;
+        static int enemiehitvalue;
         static int health;
+        static int enemiekillvalue;
+        static int killscoremultipler;
         static void Main(string[] args)
         {
             Console.WriteLine("Methods stuff");
             health = 100;
             score = 0;
+            enemiehitvalue = 25;
             enemiekillvalue = 50;
-
+            killscoremultipler = 1;
            ShowHUD();
            
             
@@ -50,11 +53,37 @@ namespace HelloWorldMethods1
             Console.WriteLine("++++++++++++++++++++++++");
             Console.WriteLine("");
             Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
+      
             Console.WriteLine("");
             Attack();
+            ShowHUD();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("You just took damage from an enemie!");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            TakeDamage();
+            ShowHUD();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("+++++++++++++++++++++++++++");
+            Console.WriteLine("You just killed the enemie!");
+            Console.WriteLine("+++++++++++++++++++++++++++");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            KillEnemie();
             ShowHUD();
 
 
@@ -68,14 +97,21 @@ namespace HelloWorldMethods1
         static void ShowHUD()
         {
             Console.WriteLine("=========================");
-            Console.WriteLine("Here's your current stats");
+            Console.WriteLine("Here's your current stats"); 
             Console.WriteLine("=========================");
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine("Health: " + health + "     Score: " + score);
+            Console.WriteLine("                Score Multiplier: " + killscoremultipler + "x");
             Console.ReadKey(true);
         }
-        
+
+
+        static void Killmultiply()
+        {
+            score = score * killscoremultipler;
+        }
+
         static void TakeDamage()
         {
             health = health - 20;
@@ -84,7 +120,14 @@ namespace HelloWorldMethods1
 
         static void Attack()
         {
-            score = score + enemiekillvalue;
+            score = score + enemiehitvalue;
+        }
+
+        static void KillEnemie()
+        {
+            killscoremultipler = killscoremultipler + 1;
+
+            score = score + (enemiekillvalue * killscoremultipler);
         }
     }
 }
